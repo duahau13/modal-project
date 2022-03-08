5d533d383d0854068e63c68a36a99b123fc1b4c0
duahau13 <56484469+duahau13@users.noreply.github.com>
2022-03-08 16:18:26 +0700

    update

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,23 @@
 <template>
   <h1>{{ title }}</h1>
-  <Modal />
+  <div v-if="showModal">
+    <Modal theme="" @close="toggleModal">
+      <h1>Sign up for entrance</h1>
+      <p>Grab your copy at half price!</p>
+      <template v-slot:links>
+        <a href="#">sign up now</a>
+        <a href="#">more info</a>
+      </template>
+    </Modal>
+  </div>
+  <div v-if="showModalTwo">
+    <Modal @close="toggleModalTwo">
+      <h1>Get the newsletter</h1>
+      <p>Latest promotion and codes.</p>
+    </Modal>
+  </div>
+  <button @click="toggleModal">Toggle modal</button>
+  <button @click="toggleModalTwo">Toggle modal</button>
 </template>
 
 <script>
@@ -11,10 +28,20 @@ export default {
   data() {
     return {
       title: "My First Vue App",
+      showModal: false,
+      showModalTwo: false,
     };
   },
   components: {
     Modal,
+  },
+  methods: {
+    toggleModal() {
+      this.showModal = !this.showModal;
+    },
+    toggleModalTwo() {
+      this.showModalTwo = !this.showModalTwo;
+    },
   },
 };
 </script>
